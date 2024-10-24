7a8ca07a4cecca185a07c30296d98a4676abe0c6
barcaca <73846225+barcaca@users.noreply.github.com>
2024-10-24 18:03:41 -0300

    feat: :lipstick: adicionar fonte e paleta de cores ao projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,16 @@
 export default {
   content: ['./src/**/*.{html,js,ts}'],
   darkMode: ['class'],
-  theme: {
-    extend: {},
+  theme: { fontFamily: {
+    sans: ['Raleway', 'system-ui', 'sans-serif'],
+    display: ['Cinzel Decorative', 'system-ui', 'sans-serif'],
+    title: ['Poppins', 'system-ui', 'sans-serif'],
+  },
+  extend: {
+    screens: {
+      xs: '450px',
+    },
+  }
   },
   plugins: [],
 }
